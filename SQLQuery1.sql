c523dcb9d9a9bb5e746004232efd48dcb25e56f2
Abdallh Mohammed <absallh43@gmail.com>
2020-03-31 10:12:38 +0200

    db

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,35 +1,35 @@
 create table person (
-	fname varchar(20) not null,
-	lname varchar(20) not null,
-	username varchar(30) not null primary key,
-	person_password varchar(10) not null unique,
-	permission varchar(50) ,
-	phoneNumber int unique
+	name varchar(20) not null,
+	username varchar(50) not null primary key,
+	password varchar(47) not null unique,
+	permission bit ,
+	phoneNumber varchar(11) unique
 );
 
 create table shiftWork (
 	startTime time not null,
 	endTime time,
 	MaxMemberNumber int,
-	shiftCost money,
-	shiftNum int not null primary key,
+	shiftCost double,
+	shiftNum varchar(50) not null primary key
 );
 
 create table trainerShift(
 	shiftNum int not null REFERENCES shiftWork (shiftNum),
 	trainerID varchar(30) not null REFERENCES person (username),
-	HoursNum int
+	HoursNum int,
+    PRIMARY KEY(shiftNum, trainerID)
 );
 
 create table bill(
-	bill_ID int not null primary key,
+	bill_ID int primary key AUTO_INCREMENT,
 	endOfTheGracePeriod time ,
-	paied money,
+	paied boolean
 );
 
 create table package(
 	discount float,
-	shiftCostAfterDiscount money,
+	shiftCostAfterDiscount float,
 	shiftNum int not null REFERENCES shiftWork (shiftNum),
 	packageName Varchar(50) not null primary key
 );
@@ -37,16 +37,18 @@ create table package(
 create table billPackage(
 	packageName varchar(50) REFERENCES package (packageName),
 	bill_ID int not null REFERENCES bill (bill_ID),
-
+    PRIMARY KEY(packageName, bill_ID)
 );
 
 create table memberAttendance(
 	member varchar(30) REFERENCES person (username),
-	attData time not null primary key,
+	attData time not null,
+    PRIMARY KEY(member, attData)
 );
 
 
 create table memberBill(
 	member varchar(30)  not null REFERENCES person (username),
 	bill_ID int REFERENCES bill (bill_ID),
-);	
+    PRIMARY KEY (member, bill_ID)
+);
--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php session_start();
+  if(isset($_SESSION['userName'])){
+    header("Location: menuBar.html");
+    exit;
+  }
+ ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,15 +23,15 @@
 <img class ="welcomephoto" src ='photos/banner.jpg'  >
 </br>
 
-<form>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
 ​<div class="container">
     <div class="form-group">
       <label for="email">Email:</label> <br>
-      <input type="email" class="form-control" id="email" placeholder="Enter email">
+      <input type="email" class="form-control" name="email" placeholder="Enter email">
     </div>
     <div class="form-group">
       <label for="pwd">Password:</label> <br>
-      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+      <input type="password" class="form-control" name="pwd" placeholder="Enter password">
     </div>
     <div class="checkbox">
       <label><input type="checkbox"> Remember me</label>
@@ -33,11 +40,21 @@
 </div>
 
 </form>
+<?php
+if (isset($_POST['email']) && isset($_POST['pwd'])){
+  if((strcmp($_POST['email'], 'absallh43@gmail.com') == 0) &&
+  (strcmp($_POST['pwd'], '123') == 0)){
+    $_SESSION['userName'] = $_POST['email'];
+    header("Location: menuBar.html");
+    exit;
+  }else{
+    echo "<script> alert('wrong Email or Password'); </script>";
+  }
+}
+?>
 <video  width =100%  autoplay>
   <source src="photos/shika's video_Small.mp4" type="video/mp4">
   <source src="photos/shika's video_Small.ogg" type="video/ogg">
-
-
 </video>
 </body>
 </html>
